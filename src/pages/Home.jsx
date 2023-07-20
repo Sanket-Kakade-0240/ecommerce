0867334f030eeba5785f5a995ae6e0974f98ac42
Sanket-Kakade-0240 <sanketskofficial0240@gmail.com>
2023-07-20 17:30:13 +0530

    pages
    pages defined

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import NavBar from '../components/NavBar'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Announcement/>
+
+    </div>
+  )
+}
+
+export default Home;
